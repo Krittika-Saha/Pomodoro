734dea4fb0c9d43c5171ac8eb6e37da31782be84
Krittika-Saha <krittika.saha.dev@gmail.com>
2020-12-20 21:30:46 +0530

    Added a minute facility

--- main.py
+++ main.py
@@ -14,12 +14,19 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_command():
-  count_down(5)
+  count_down(62)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-  canvas.itemconfig(timer_text, text=count)
+
+  count_min = count // 60
+  count_sec = count % 60 
+  if count_sec < 10 and count_sec >= 0:
+    count_sec = f'0{count_sec}'
+  canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
   if count > 0:
-    window.after(1000, count_down, count - 1)
+    window.after(1000, count_down, count - 1) 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Pomodoro')
